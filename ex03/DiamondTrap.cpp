f320baa4462da35e0826d22b0f864982dd31fbf5
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-10-29 20:07:05 +0100

    partial diamond trap

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,39 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:22:43 by jeberle           #+#    #+#             */
-/*   Updated: 2024/10/29 17:22:44 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/10/29 19:33:33 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap() : ClapTrap("default"), ScavTrap("default"), DiamondTrap("default"), name("default") {
+	this->hit_points + DiamondTrap::hit_points;
+	this->energy_points + ScavTrap::energy_points;
+	this->attack_damage + DiamondTrap::attack_damage;
+	std::cout << "DiamondTrap default constructor called" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), ScavTrap(name), DiamondTrap(name), name(name) {
+	this->hit_points + DiamondTrap::hit_points;
+	this->energy_points + ScavTrap::energy_points;
+	this->attack_damage + DiamondTrap::attack_damage;
+	std::cout << "DiamondTrap named constructor called for " << name << std::endl;
+}
+
+DiamondTrap& DiamondTrap::operator=(const DiamondTrap& src) {
+	std::cout << "DiamondTrap assignment operator called" << std::endl;
+	if (this != &src) {
+		ClapTrap::operator=(src);
+		this->name = src.name;
+	}
+	return *this;
+}
+
+DiamondTrap::~DiamondTrap() {
+	std::cout << "DiamondTrap destructor called for " << name << std::endl;
+}
+
+void	DiamondTrap::~DiamondTrap() {
+	std::cout << "DiamondTrap destructor called for " << name << std::endl;
+}
--- ex03/DiamondTrap.hpp
+++ ex03/DiamondTrap.hpp
@@ -6,7 +6,29 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:22:44 by jeberle           #+#    #+#             */
-/*   Updated: 2024/10/29 17:22:50 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/10/29 19:18:10 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef DIAMONDTRAP_HPP
+# define DIAMONDTRAP_HPP
+
+#include <string>
+#include <iostream>
+#include "DiamondTrap.hpp"
+#include "ScavTrap.hpp"
+
+class DiamondTrap : virtual public DiamondTrap, virtual public ScavTrap {
+	private:
+		std::string	name;
+	public:
+		DiamondTrap();
+		DiamondTrap(std::string name);
+		DiamondTrap(const DiamondTrap& src);
+		DiamondTrap& operator=(const DiamondTrap& src);
+		~DiamondTrap();
+		void	attack(const std::string& target);
+		void	whoAmI(void);
+};
+
+#endif
